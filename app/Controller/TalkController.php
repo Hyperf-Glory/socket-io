5ade024edbe330f0cd46474f4bd227e9a6273dbb
何平 <847050412@qq.com>
2020-11-12 23:04:57 +0800

    TODO

---
+++ app/Controller/TalkController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+class TalkController extends AbstractController
+{
+    /**
+     * //TODO 11.13日需要完成的
+     */
+    public function list()
+    {
+
+    }
+}
---
+++ app/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+class UserController extends AbstractController
+{
+    /**
+     * //TODO 11.13日需要完成的
+     */
+    public function setting()
+    {
+
+    }
+
+    /**
+     * //TODO 11.13日需要完成的
+     */
+    public function friendApplyNum()
+    {
+
+    }
+}
