8fb5aaaa7656df15da99c425f6f70432ff6037ef
何平 <847050412@qq.com>
2020-11-12 22:51:56 +0800

    完成服务端对客户端事件的监听

--- app/Controller/WebSocketController.php
+++ app/Controller/WebSocketController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace App\Controller;
 
-use App\Component\MessageParser;
 use Hyperf\SocketIOServer\Annotation\Event;
 use Hyperf\SocketIOServer\Annotation\SocketIONamespace;
 use Hyperf\SocketIOServer\BaseNamespace;
@@ -13,61 +12,33 @@ use Hyperf\Utils\Codec\Json;
 
 /**
  * @example
- * 42/socket.io/,["event",{"projectId":"88","theme":"getUserInfo","time":10000}]
  * @SocketIONamespace("/")
  */
 class WebSocketController extends BaseNamespace
 {
     /**
-     * @Event("event")
+     * @Event("event_keyboard")
      * @param \Hyperf\SocketIOServer\Socket $socket
      *
      * @return string
      */
-    public function onEvent(\Hyperf\SocketIOServer\Socket $socket, $data)
+    public function onEventKeyboard(\Hyperf\SocketIOServer\Socket $socket, $data)
     {
-        dump($data);
-        $socket->emit('event', $socket->getSid() . " say: {}");
+        $socket->emit('chat_message', $socket->getSid() . " say: {}");
         // 应答
         return 'Event Received: string';
     }
 
-    /**
-     * @Event("join-room")
-     * @param \Hyperf\SocketIOServer\Socket $socket
-     * @param string                        $data
-     */
-    public function onJoinRoom(Socket $socket, $data)
-    {
-        // 将当前用户加入房间
-        $socket->join($data);
-        // 向房间内其他用户推送（不含当前用户）
-        $socket->to($data)->emit('event', $socket->getSid() . "has joined {$data}");
-        // 向房间内所有人广播（含当前用户）
-        $this->emit('event', 'There are ' . count($socket->getAdapter()->clients($data)) . " players in {$data}");
-    }
 
     /**
-     * @Event("say")
+     * @Event("event_talk")
      * @param \Hyperf\SocketIOServer\Socket $socket
      * @param string                        $data
      */
-    public function onSay(Socket $socket, $data)
+    public function onEventTalk(Socket $socket, $data)
     {
         $data = Json::decode($data);
         $socket->to($data['room'])->emit('event', $socket->getSid() . " say: {$data['message']}");
     }
 
-    /**
-     * @Event("leave")
-     * @param \Hyperf\SocketIOServer\Socket $socket
-     * @param                               $data
-     */
-    public function onLeave(Socket $socket, $data)
-    {
-        $data  = MessageParser::decode($data);
-        $sid   = $data['sid'];
-        $rooms = $socket->getAdapter()->clientRooms($sid);
-        $socket->getAdapter()->del($sid, implode(",", $rooms));
-    }
 }
