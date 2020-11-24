d510ba25c49f1a30605654e5c030e95e9c4ac5e1
何平 <847050412@qq.com>
2020-11-24 15:39:12 +0800

    commit

--- app/Amqp/Consumer/ChatConsumer.php
+++ app/Amqp/Consumer/ChatConsumer.php
@@ -1,33 +1,33 @@
 <?php
-//
-//declare(strict_types=1);
-///**
-// * This file is part of Hyperf.
-// *
-// * @link     https://www.hyperf.io
-// * @document https://hyperf.wiki
-// * @contact  group@hyperf.io
-// * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
-// */
-//namespace App\Amqp\Consumer;
-//
-//use Hyperf\Amqp\Annotation\Consumer;
-//use Hyperf\Amqp\Message\ConsumerMessage;
-//use Hyperf\Amqp\Result;
-//use Hyperf\Utils\Coroutine;
-//use PhpAmqpLib\Message\AMQPMessage;
-//
-///**
-// * @Consumer(exchange="hyperf", routingKey="hyperf", queue="hyperf", name="ChatConsumer", nums=1)
-// */
-//class ChatConsumer extends ConsumerMessage
-//{
-//    public function consumeMessage($data, AMQPMessage $message): string
-//    {
-//        Coroutine::create(function () use ($data) {
-//            [$packet, $opts] = unserialize($data->payload);
-//            $this->doBroadcast($packet, $opts);
-//        });
-//        return Result::ACK;
-//    }
-//}
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace App\Amqp\Consumer;
+
+use Hyperf\Amqp\Annotation\Consumer;
+use Hyperf\Amqp\Message\ConsumerMessage;
+use Hyperf\Amqp\Result;
+use Hyperf\Utils\Coroutine;
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * @Consumer(exchange="hyperf", routingKey="hyperf", queue="hyperf", name="ChatConsumer", nums=1)
+ */
+class ChatConsumer extends ConsumerMessage
+{
+    public function consumeMessage($data, AMQPMessage $message): string
+    {
+        Coroutine::create(function () use ($data) {
+            [$packet, $opts] = unserialize($data->payload);
+            $this->doBroadcast($packet, $opts);
+        });
+        return Result::ACK;
+    }
+}
