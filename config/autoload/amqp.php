658f6ecf77c8bcbcc16432ac4280de824ee1357a
何平 <847050412@qq.com>
2020-11-24 22:07:12 +0800

    commit

--- config/autoload/amqp.php
+++ config/autoload/amqp.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  */
 return [
     'default' => [
-        'host' => 'localhost',
+        'host' => 'host.docker.internal',
         'port' => 5672,
         'user' => 'guest',
         'password' => 'guest',
