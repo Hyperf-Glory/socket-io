63885795b3c2a8772d7159c27b236ef1056209b5
he ping <847050412@qq.com>
2020-11-25 21:11:58 +0800

    暂时停止更新

--- config/autoload/nsq.php
+++ config/autoload/nsq.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 /**
  * This file is part of Hyperf.
  *
@@ -12,20 +12,28 @@ declare(strict_types=1);
 return [
     'default' => [
         'enable' => true,
-        'host' => env('NSQ_HOST','localhost'),
-        'port' => 4150,
-        'pool' => [
+        'host'   => env('NSQ_HOST', 'localhost'),
+        'port'   => 4150,
+        'pool'   => [
             'min_connections' => 1,
             'max_connections' => 10,
             'connect_timeout' => 10.0,
-            'wait_timeout' => 3.0,
-            'heartbeat' => -1,
-            'max_idle_time' => 60.0,
+            'wait_timeout'    => 3.0,
+            'heartbeat'       => -1,
+            'max_idle_time'   => 60.0,
         ],
-        'nsqd' => [
-            'port' => 4151,
-            'options' => [
+        'nsqd'   => [
+            'port'     => 4151,
+            'options'  => [
+                'base_uri' => env('NSQ_HOST', 'localhost'),
             ],
+        ],
+    ],
+    'nsqd'   => [
+        'host'   => env('NSQ_HOST', 'localhost'),
+        'port'     => 4151,
+        'options'  => [
+            'base_uri' => env('NSQ_HOST', 'localhost'),
         ],
     ],
 ];
