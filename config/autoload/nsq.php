93fa3dde3dc53c1d45b2f002bc5f7aadc4150da7
he ping <847050412@qq.com>
2020-11-25 17:05:32 +0800

    fix

--- config/autoload/nsq.php
+++ config/autoload/nsq.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 return [
     'default' => [
         'enable' => true,
-        'host' => env('NSQ_HOST','127.0.0.1'),
+        'host' => env('NSQ_HOST','chat_nsq'),
         'port' => 4150,
         'pool' => [
             'min_connections' => 1,
