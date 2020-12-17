08c132f3aca126a283fc694cea8360e15f8e044b
he ping <847050412@qq.com>
2020-12-17 13:32:07 +0800

    自己封装

---
+++ app/Component/Jwt.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+/**
+ *
+ * This file is part of the My App.
+ *
+ * Copyright CodingHePing 2016-2020.
+ *
+ * This is my open source code, please do not use it for commercial applications.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ *
+ * @author CodingHePing<847050412@qq.com>
+ * @link   https://github.com/codingheping/hyperf-chat-upgrade
+ */
+namespace App\Component;
+
+/**
+ * Class Jwt.
+ * @TODO 待封装
+ */
+class Jwt
+{
+}
