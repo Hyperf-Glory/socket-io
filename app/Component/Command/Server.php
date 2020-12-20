13a6e19d00b81f758165137939143b1ab60db3f0
he ping <847050412@qq.com>
2020-12-20 22:39:52 +0800

    commit

---
+++ app/Component/Command/Server.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+/**
+ *
+ * This file is part of the My App.
+ *
+ * Copyright CodingHePing 2016-2020.
+ *
+ * This is my open source code, please do not use it for commercial applications.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ *
+ * @author CodingHePing<847050412@qq.com>
+ * @link   https://github.com/codingheping/hyperf-chat-upgrade
+ */
+namespace App\Component\Command;
+
+class Server
+{
+    public const LOGO = <<<'EOF'
+                            _______  _______  _______  _______ 
+|\     /||\     /|(  ____ )(  ____ \(  ____ )(  ____ \        |
+| )   ( |( \   / )| (    )|| (    \/| (    )|| (    \/        | 
+| (___) | \ (_) / | (____)|| (__    | (____)|| (__            | 
+|  ___  |  \   /  |  _____)|  __)   |     __)|  __)           |
+| (   ) |   ) (   | (      | (      | (\ (   | (              |
+| )   ( |   | |   | )      | (____/\| ) \ \__| )              |
+|/     \|   \_/   |/       (_______/|/   \__/|/               |
+                                                      
+ _______           _______ _________                          |
+(  ____ \|\     /|(  ___  )\__   __/                          |
+| (    \/| )   ( || (   ) |   ) (                             |
+| |      | (___) || (___) |   | |                             |
+| |      |  ___  ||  ___  |   | |                             |
+| |      | (   ) || (   ) |   | |                             |
+| (____/\| )   ( || )   ( |   | |                             |
+(_______/|/     \||/     \|   )_(   
+EOF;
+
+
+}
