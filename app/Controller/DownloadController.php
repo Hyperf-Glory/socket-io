d0122fe5985e00dca0c4cfc128e6d8a2e7c3436f
he ping <847050412@qq.com>
2020-12-25 15:57:48 +0800

    Add Download

---
+++ app/Controller/DownloadController.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+/**
+ *
+ * This file is part of the My App.
+ *
+ * Copyright CodingHePing 2016-2020.
+ *
+ * This is my open source code, please do not use it for commercial applications.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ *
+ * @author CodingHePing<847050412@qq.com>
+ * @link   https://github.com/codingheping/hyperf-chat-upgrade
+ */
+namespace App\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+
+class DownloadController extends AbstractController
+{
+    public function userChatFile(): ResponseInterface
+    {
+    }
+
+    public function download(): ResponseInterface
+    {
+    }
+}
