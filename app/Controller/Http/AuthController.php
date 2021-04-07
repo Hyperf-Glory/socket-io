f815ae48d7dbe68f8bc04c0fc965782c5235b96a
he ping <847050412@qq.com>
2021-04-07 13:51:42 +0800

    Auth

--- app/Controller/Http/AuthController.php
+++ app/Controller/Http/AuthController.php
@@ -3,9 +3,34 @@ declare(strict_types = 1);
 namespace App\Controller\Http;
 
 use App\Controller\AbstractController;
+use Psr\Http\Message\ResponseInterface;
 
 class AuthController extends AbstractController
 {
 
+    public function register() : ResponseInterface
+    {
+
+    }
+
+    public function login() : ResponseInterface
+    {
+
+    }
+
+    public function logout() : ResponseInterface
+    {
+
+    }
+
+    public function sendVerifyCode() : ResponseInterface
+    {
+
+    }
+
+    public function forgetPassword() : ResponseInterface
+    {
+
+    }
 }
 
